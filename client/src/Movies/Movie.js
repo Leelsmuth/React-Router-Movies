c9e9b8261ad67da1a37a616c4fdb1f8c614eefde
Leelsmuth <femianjorin@rocketmail.com>
2019-09-17 14:58:17 +0100

    minor refactor for movie.js

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -3,9 +3,9 @@ import axios from "axios";
 
 const Movie = props => {
   const [movie, setMovie] = useState();
+  const id = props.match.params.id;
 
   useEffect(() => {
-    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -17,7 +17,7 @@ const Movie = props => {
       .catch(error => {
         console.error(error);
       });
-  }, [props.match.params.id]);
+  }, [id]);
 
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
